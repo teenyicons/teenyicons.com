755f196fa108e9a4cb608f15095ef94a09b9ddcf
smhmd <contact.simohamed@gmail.com>
2020-07-22 16:19:51 +0100

    enhance open graph details

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,30 +5,78 @@ export default {
     htmlAttrs: {
       lang: 'en',
     },
-    title: 'Teenyicons',
+    title: 'Teenyicons — Tiny minimal 1px icons',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       {
-        hid: 'description',
-        name: 'description',
-        content: process.env.npm_package_description || 'Tiny minimal 1px icons',
+        name: 'title',
+        content: 'Teenyicons — Tiny minimal 1px icons',
       },
       {
-        property: 'og:image',
-        content: '/og.png',
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || 'An elegant icon set by Anja van Staden with more than a thousand icons.',
+      },
+      {
+        property: '',
+        content: '',
+      },
+      {
+        property: 'og:type',
+        content: 'website',
       },
       {
         property: 'og:url',
         content: 'https://teenyicons.com',
       },
       {
+        property: 'og:title',
+        content: 'Teenyicons — Tiny minimal 1px icons',
+      },
+      {
+        property: 'og:description',
+        content: 'An elegant icon set by Anja van Staden with more than a thousand icons.',
+      },
+      {
+        property: 'og:image',
+        content: '/og.png',
+      },
+      {
+        property: 'og:image:width',
+        content: '1200',
+      },
+      {
+        property: 'og:image:height',
+        content: '1200',
+      },
+      {
         name: 'twitter:card',
         content: 'summary_large_image',
       },
       {
-        property: 'og:site_name',
-        content: 'Teenyicons',
+        name: 'twitter:creator',
+        content: '@_smhmd',
+      },
+      {
+        name: 'twitter:site',
+        content: '@teenyicon',
+      },
+      {
+        name: 'twitter:title',
+        content: 'Teenyicons — Tiny minimal 1px icons',
+      },
+      {
+        property: 'twitter:description',
+        content: 'An elegant icon set by Anja van Staden with more than a thousand icons.',
+      },
+      {
+        name: 'twitter:url',
+        content: 'https://teenyicons.com',
+      },
+      {
+        property: 'twitter:image',
+        content: '/og.png',
       },
       {
         name: 'twitter:image:alt',
