513e0bb03eb9258ef7cb5d591b860cade4bd8703
smhmd <contact.simohamed@gmail.com>
2020-07-29 14:29:18 +0100

    add proper icon dark mode

--- components/AppIcon.vue
+++ components/AppIcon.vue
@@ -1,16 +1,17 @@
 <template>
   <div
     :title="icon"
-    class="relative flex flex-col items-center justify-center py-8 space-y-2 border border-transparent rounded cursor-pointer icon-card hover:border-theme-secondary hover:shadow-mermaid"
+    class="relative flex flex-col items-center justify-center py-8 space-y-2 border border-transparent rounded cursor-pointer hover:border-theme-secondary hover:shadow-mermaid"
     :class="copied ? 'border-theme-secondary shadow-mermaid' : ''"
     @click="copy"
     @keydown.enter="copy"
     @keydown.space.prevent="copy"
   >
     <component :is="icon" ref="icon" :width="size" :height="size" />
-    <span class="w-full px-1 text-xs text-center truncat">{{
-      icon.replace(variant + '-', '')
-    }}</span>
+    <span
+      class="w-full px-1 text-xs text-center text-theme-secondary truncat"
+      >{{ icon.replace(variant + '-', '') }}</span
+    >
     <span
       v-show="copied"
       class="absolute inset-x-0 bottom-0 w-full mx-auto text-xs text-center text-black rounded-b bg-mermaid"
